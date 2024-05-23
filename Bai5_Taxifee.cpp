e2c16b40019ae5c3ccb5a91e4c9005bb7221e586
tiencao75 <caotien20000@gmail.com>
2024-05-23 11:16:38 +0700

    Add GIA_4 =10000

--- Bai5_Taxifee.cpp
+++ Bai5_Taxifee.cpp
@@ -6,6 +6,7 @@ void taxifee(float so_km ){
 	const int GIA_1(5000);
 	const int GIA_2(4000);
 	const int GIA_3(6000);
+    const int GIA_4(10000);
     float tong;
     if (so_km <= NGUONG_1){
         printf("Tien cuoc la: %0.3f\n", tong = so_km *GIA_1);
@@ -27,4 +28,4 @@ taxifee(0.5);
     return 0;
 }
 
-// Add new comment and change the NGUONG_2
+// Add GIA_4 = 10000
